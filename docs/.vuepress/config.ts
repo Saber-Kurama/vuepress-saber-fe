337f01a5d081d78773d53ab54327cd18bdfb8b1a
guochao <463561248@qq.com>
2022-03-12 21:48:48 +0800

    ✨ feat:  添加主题

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,8 +6,11 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1, shrink-to-fit=no' }],
+  ],
   // // 主题和它的配置
-  theme: '@vuepress/theme-default',
+  theme: '@vuesax/vuepress-theme-vuesax',
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
   },
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,4 @@
-# saber
+---
+home: true
+heroText: <b>Vuesax</b> Framework <br> Components for <a target="_blank" href="https://vuejs.org/" >Vuejs</a>
+---
