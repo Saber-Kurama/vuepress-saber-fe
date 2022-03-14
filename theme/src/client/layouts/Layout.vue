f38711e6886b798c667ed41994d6b903979d039a
guochao <463561248@qq.com>
2022-03-14 22:05:35 +0800

    ✨ feat:  简单配置

--- theme/src/client/layouts/Layout.vue
+++ theme/src/client/layouts/Layout.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
+import { useRouter } from 'vue-router'
+// close sidebar after navigation
+let unregisterRouterHook
+onMounted(() => {
+  const router = useRouter()
+  unregisterRouterHook = router.afterEach(() => {
+    // toggleSidebar(false)
+  })
+})
+onUnmounted(() => {
+  unregisterRouterHook()
+})
+</script>
 <template>
   <div>
     <Content />
